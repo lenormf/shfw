a17154d21e4eb04baeee97cd8c7f5101edf7bb60
Frank LENORMAND <lenormf@gmail.com>
2013-06-04 21:01:35 +0200

    Added an associative array detection function, improved the arrayForEach

--- array.bash
+++ array.bash
@@ -15,8 +15,13 @@ function arrayJoin {
 	echo "$res"
 }
 
+function arrayIsAssociative {
+	declare -A | grep -qw "$1"
+	return
+}
+
 function arrayForEach {
-	for i in $(eval "echo \${${1}[@]}"); do
-		$2 "$i"
+	for i in $(eval "echo \${!${1}[@]}"); do
+		$2 "$i" $(eval "echo \${${1}[$i]}")
 	done
 }
