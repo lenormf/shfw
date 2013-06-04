eab21f238a3a646027d2bcc76af20a82e8693125
Frank LENORMAND <lenormf@gmail.com>
2013-06-04 21:17:04 +0200

    Fixed the urlEncode function

--- url.bash
+++ url.bash
@@ -3,17 +3,17 @@
 ## by lenormf
 ##
 
-RESERVED_CHARS="#$&'+,/:;=?@[]"
+RESERVED_CHARS=$'#$&\'+,/:;=?@[]'
 
 function urlEncode {
-	local s="$1"
+	local s="$@"
 
 	for i in $(seq 0 $((${#RESERVED_CHARS} - 1))); do
 		local c="${RESERVED_CHARS:$i:1}"
-		local hex=$(printf '%X' $(strAsc "$c"))
+		local hex=$(printf '%X' $(printf "%d" "'$c"))
 
 		test "$c" = "'" || c="\\${c}"
-		s=$(echo "$s" | sed "s/${c}/%${hex}/g")
+		s=${s//${c}/%${hex}}
 	done
 
 	echo "$s"
