c9e546c116745e1aea717cba7a0c2091a8f0d2af
Frank LENORMAND <lenormf@gmail.com>
2013-06-16 17:07:40 +0200

    Implemented an URL decode function

--- url.bash
+++ url.bash
@@ -3,14 +3,18 @@
 ## by lenormf
 ##
 
-RESERVED_CHARS=$'#$&\'+,/:;=?@[]'
+INCLUDE_PATH=$(dirname "$_")
+
+source "${INCLUDE_PATH}/string.bash"
+
+RESERVED_CHARS=$'#$&\'+,/:;=?@[]- '
 
 function urlEncode {
 	local s="$@"
 
 	for i in $(seq 0 $((${#RESERVED_CHARS} - 1))); do
 		local c="${RESERVED_CHARS:$i:1}"
-		local hex=$(printf '%X' $(printf "%d" "'$c"))
+		local hex=$(strHex $(strAsc "$c"))
 
 		test "$c" = "'" || c="\\${c}"
 		s=${s//${c}/%${hex}}
@@ -20,5 +24,13 @@ function urlEncode {
 }
 
 function urlDecode {
-	echo
+	local s="$@"
+	local t=( $(echo "$s" | egrep -o '%[A-Z0-9]{2}' | uniq) )
+
+	for i in "${t[@]}"; do
+		local l=$(printf '\x'${i:1:2}'\n')
+		s=$(echo "$s" | sed "s/${i}/${l}/g")
+	done
+
+	echo "$s"
 }
