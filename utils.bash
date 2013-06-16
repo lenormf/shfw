f03dac94710b1eaa7a33b9527937d074f8431c12
Frank LENORMAND <lenormf@gmail.com>
2013-06-16 17:08:00 +0200

    Added an hidden input utility function

--- utils.bash
+++ utils.bash
@@ -3,12 +3,29 @@
 ## by lenormf
 ##
 
+INCLUDE_PATH=$(dirname "$_")
+
+source "${INCLUDE_PATH}/string.bash"
+
 function enum {
 	local n=0
 	for i in "$@"; do
-		eval "${i}=${n}"
+		local up=$(strToUpper "$i")
+
+		eval "${up}=${n}"
 		n=$((n + 1))
 	done
+}
+
+function hiddenInput {
+	local old=$(stty -g)
+
+	test $# -gt 0 && echo -n "$@" >&2
+	stty -echo
+	read _data
+	stty "$old"
+
+	echo "$_data"
 }
 
 function atexit {
